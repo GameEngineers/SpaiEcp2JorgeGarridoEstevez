1ded30a0728f68bf102af23c49f9b850f90ae900
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2015-10-06 09:01:47 +0200

    Feature#07 : Metodo añadido UpperFullName

--- src/test/java/es/upm/miw/spai/ecp2/UserTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -9,41 +9,46 @@ import es.upm.miw.spai.ecp2.User;
 
 public class UserTest {
 
-    private User u;
-    
-    @Before
-    public void before(){
-        u = new User(1234, "Jorge", "garrido");
-    }
-    
-    @Test
-    public void testFullName(){
-        assertEquals("Jorge Garrido",u.fullName());
-    }
-    
-    @Test
-    public void testInitials(){
-        assertEquals("J.", u.initials());
-    }
-    
-    @Test 
-    public void testGetNumber(){
-        assertEquals(1234, u.getNumber());
-    }
-    
-    @Test 
-    public void testGetName(){
-        assertEquals("Jorge", u.getName());
-    }
-    
-    @Test
-    public void testGetFamilyName(){
-        assertEquals("Garrido", u.getFamilyName());
-    }
-    
-    @Test
-    public void testAbbrFullName(){
-        assertEquals("J. Garrido", u.AbbrFullName());
-    }
+	private User u;
+
+	@Before
+	public void before() {
+		u = new User(1234, "Jorge", "garrido");
+	}
+
+	@Test
+	public void testFullName() {
+		assertEquals("Jorge Garrido", u.fullName());
+	}
+
+	@Test
+	public void testInitials() {
+		assertEquals("J.", u.initials());
+	}
+
+	@Test
+	public void testGetNumber() {
+		assertEquals(1234, u.getNumber());
+	}
+
+	@Test
+	public void testGetName() {
+		assertEquals("Jorge", u.getName());
+	}
+
+	@Test
+	public void testGetFamilyName() {
+		assertEquals("Garrido", u.getFamilyName());
+	}
+
+	@Test
+	public void testAbbrFullName() {
+		assertEquals("J. Garrido", u.AbbrFullName());
+	}
+
+	@Test
+    public void testUpperFullName() {
+        assertEquals("JORGE", u.upperFullName());
+	}
    
 }
